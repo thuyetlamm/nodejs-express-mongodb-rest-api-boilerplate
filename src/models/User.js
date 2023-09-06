61a798a14117ab083b367ea087b0542d8b1bd0cc
thuyetlamm <thuyetlamm@gmail.com>
2023-09-06 12:50:39 +0700

    fix avartar length

--- src/models/User.js
+++ src/models/User.js
@@ -9,7 +9,7 @@ const User = new Schema(
     avatar: { type: String, maxLength: 100 },
     address: { type: String, maxLength: 255 },
     email: { type: String, maxLength: 100, unique: true },
-    password: { type: String, maxLength: 50 },
+    password: { type: String, maxLength: 150 },
     type: { type: Number, default: 1 },
     status: { type: Number, default: 1 },
   },
