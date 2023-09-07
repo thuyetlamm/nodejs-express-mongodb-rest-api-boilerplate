2d977fbaffb30f97afe2149429c4190da2b10980
thuyetlamm <thuyetlamm@gmail.com>
2023-09-07 22:46:11 +0700

    update reason

--- src/models/Bol.js
+++ src/models/Bol.js
@@ -3,15 +3,12 @@ const timeZone = require("mongoose-timezone");
 
 const Bol = new Schema(
   {
-    code: { type: String, maxLength: 50, required: true, unique: true },
+    code: { type: String, maxLength: 20, required: true, unique: true },
     from: { type: String, maxLength: 50, required: true },
-    city: { type: String, maxLength: 100 },
     address: { type: String, maxLength: 100, require: true },
     userName: { type: String, maxLength: 255, default: "" },
     receivedName: { type: String, maxLength: 50, default: "" },
     receivedPhoneNumber: { type: String, maxLength: 12, default: "" },
-    ward: { type: String, maxLength: 100, default: "" },
-    distrist: { type: String, maxLength: 100, default: "" },
     customerCode: { type: String, required: true },
     customerName: { type: String, default: "" },
     customerId: { type: String, required: true },
@@ -22,7 +19,7 @@ const Bol = new Schema(
     startDate: { type: Date, default: new Date() },
     endDate: { type: Date, default: null },
     category: { type: Array, required: true },
-    remark: { type: String, default: "" },
+    reason: { type: Array, default: [] },
   },
   {
     timestamps: true,
