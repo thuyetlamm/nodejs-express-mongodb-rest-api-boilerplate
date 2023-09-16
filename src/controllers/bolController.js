fe11f22428e47489f2ebd1a6c94f2171edb20467
thuyetlamm <thuyetlamm@gmail.com>
2023-09-16 09:07:35 +0700

    update service bol

--- src/controllers/bolController.js
+++ src/controllers/bolController.js
@@ -1,78 +1,24 @@
-const XLSX = require("xlsx");
 const { Bols } = require("~/models/Bol");
 const moment = require("moment");
 const { UTC_TIMEZONES, timestamp, FORMAT_DATE } = require("~/utils/constants");
 const { BOL_STATUS_ENUM, CATEGORY_LIST } = require("~/types/bols");
 const { Customers } = require("~/models/Customer");
+const { BolServices } = require("~/services/bolServices");
 
 const NUMBER_COL = 6;
 
 class BolController {
   //[GET] /bols
   async index(req, res, next) {
-    const {
-      limit = 10,
-      page = 1,
-      keyword,
-      status = -1,
-      customerCode,
-      from,
-      to,
-    } = req.query;
     try {
-      // Convert params to need type
-      const fromDate = from
-        ? moment(from).format(FORMAT_DATE.YMD)
-        : moment().subtract(6, "days").format(FORMAT_DATE.YMD);
-      const toDate = to
-        ? moment(to).add(1, "day").format(FORMAT_DATE.YMD)
-        : moment().add(1, "day").format(FORMAT_DATE.YMD);
-
-      // QUERY OBJECTS
-      const query = {
-        $or: [
-          {
-            code: { $regex: keyword || "", $options: "i" },
-          },
-        ],
-        createdAt: {
-          $gte: fromDate,
-          $lte: toDate,
-        },
-      };
-
-      if (+status !== -1) {
-        query.status = +status;
-      }
-
-      if (customerCode) {
-        query.customerCode = customerCode;
-      }
-
-      const bols = await Bols.find(query)
-        .limit(+limit)
-        .skip(+limit * (+page - 1))
-        .sort([["updatedAt", -1]]);
-
-      const { status: spreadStatus, ...allQuery } = query;
-
-      const totalBol = await Bols.countDocuments(allQuery);
-      const totalNew = await Bols.countDocuments({
-        ...query,
-        status: BOL_STATUS_ENUM.NEW,
-      });
-      const totalRefund = await Bols.countDocuments({
-        ...query,
-        status: BOL_STATUS_ENUM.REFURNING,
-      });
-      const totalFinish = await Bols.countDocuments({
-        ...query,
-        status: BOL_STATUS_ENUM.FINISHED,
-      });
-      const totalUnsuccess = await Bols.countDocuments({
-        ...query,
-        status: BOL_STATUS_ENUM.UNSCCESSFUL,
-      });
+      const {
+        bols,
+        totalBol,
+        totalFinish,
+        totalNew,
+        totalRefund,
+        totalUnsuccess,
+      } = await BolServices.list(req.query);
 
       res.status(200).json({
         data: bols,
@@ -152,30 +98,8 @@ class BolController {
       const { id } = req.params;
 
       // Update bol
-      const payload = req.body;
-      const startDate = moment(payload.startDate)
-        .subtract(1, "day")
-        .format(FORMAT_DATE.YMDHm);
-      const endDate = payload?.endDate
-        ? moment(payload.endDate).format(FORMAT_DATE.YMDHm)
-        : null;
+      await BolServices.update(id, req.body);
 
-      const currentCustomer = await Customers.findOne({
-        _id: payload.customerId,
-      });
-
-      const convertPayload = {
-        ...payload,
-        customerCode: currentCustomer.code,
-        customerName: currentCustomer.name,
-        startDate,
-        endDate,
-      };
-      const bolDetail = await Bols.findOneAndUpdate(
-        { _id: id },
-        { $set: convertPayload },
-        { upsert: true }
-      );
       res.json({
         status: 200,
         message: "Bol updated successfully",
@@ -206,25 +130,7 @@ class BolController {
   //[POST] /BOLS/create
   async store(req, res, next) {
     try {
-      const payload = req.body;
-      const startDate = moment(payload.startDate).format("YYYY-MM-DD HH:mm");
-      const endDate = payload.endDate
-        ? moment(payload.startDate).format("YYYY-MM-DD HH:mm")
-        : null;
-
-      const currentCustomer = await Customers.findOne({
-        _id: payload.customerId,
-      });
-
-      const convertPayload = {
-        ...payload,
-        customerCode: currentCustomer.code,
-        customerName: currentCustomer.name,
-        startDate,
-        endDate,
-      };
-
-      const bol = await Bols.create(convertPayload);
+      const bol = BolServices.store(req.body);
 
       res.json({
         status: 200,
@@ -247,62 +153,7 @@ class BolController {
           message: "File not found",
         });
 
-      const customerList = await Customers.find({});
-
-      const workBook = XLSX.read(req.file.buffer, { cellDates: true });
-      const wordSheet = workBook.Sheets[workBook.SheetNames[0]];
-      const countRow = XLSX.utils.sheet_to_json(wordSheet);
-      const arrayPayload = [];
-
-      for (let index = 2; index <= countRow.length + 1; index++) {
-        const startDate = wordSheet[`A${index}`]?.v || moment().format();
-        const code = wordSheet[`B${index}`]?.v || "";
-        const customerCode = wordSheet[`C${index}`]?.v || "";
-        const receivedName = wordSheet[`D${index}`]?.v || "";
-        const receivedPhoneNumber = wordSheet[`E${index}`]?.v || "";
-        const address = wordSheet[`F${index}`]?.v || "";
-        const category = wordSheet[`G${index}`]?.v || "";
-        const quantity = wordSheet[`H${index}`]?.v || 1;
-        const convertCategoryList = category.split("+") || [];
-
-        const categoryAfterConvertToObject = convertCategoryList?.reduce(
-          (acc, category) => {
-            const findCategory = CATEGORY_LIST.find((item) =>
-              item.code.includes(category.toUpperCase())
-            );
-            if (findCategory) {
-              return [...acc, findCategory];
-            }
-            return acc;
-          },
-          []
-        );
-
-        const currentCustomer = customerList.find(
-          (c) => c.code === customerCode
-        );
-
-        arrayPayload.push({
-          code,
-          category: categoryAfterConvertToObject,
-          address,
-          quantity,
-          from: "2/10 Hồng Hà,p2,Tân Bình,HCM",
-          path: "",
-          description: "",
-          receivedName,
-          receivedPhoneNumber,
-          startDate: moment(startDate)
-            .tz(UTC_TIMEZONES)
-            .format("YYYY-MM-DD HH:mm"),
-          customerCode,
-          customerId: `${currentCustomer?._id}`,
-          customerName: currentCustomer?.name,
-          status: 0,
-        });
-      }
-
-      const bols = await Bols.insertMany(arrayPayload);
+      await BolServices.update(req.file.buffer);
 
       res.status(200).json({
         error: 0,
---
+++ src/services/bolServices.js
@@ -0,0 +1,192 @@
+const XLSX = require("xlsx");
+
+import moment from "moment";
+import { Customers } from "~/models/Customer";
+import { FORMAT_DATE, UTC_TIMEZONES } from "~/utils/constants";
+
+const { Bols } = require("~/models/Bol");
+const { BOL_STATUS_ENUM, CATEGORY_LIST } = require("~/types/bols");
+
+class BolServices {
+  list = async (query) => {
+    const {
+      limit = 10,
+      page = 1,
+      keyword,
+      status = -1,
+      customerCode,
+      from,
+      to,
+    } = query;
+
+    // Convert params to need type
+    const fromDate = from
+      ? moment(from).format(FORMAT_DATE.YMD)
+      : moment().subtract(6, "days").format(FORMAT_DATE.YMD);
+    const toDate = to
+      ? moment(to).add(1, "day").format(FORMAT_DATE.YMD)
+      : moment().add(1, "day").format(FORMAT_DATE.YMD);
+
+    // QUERY OBJECTS
+    const query = {
+      $or: [
+        {
+          code: { $regex: keyword || "", $options: "i" },
+        },
+      ],
+      createdAt: {
+        $gte: fromDate,
+        $lte: toDate,
+      },
+    };
+
+    if (+status !== -1) {
+      query.status = +status;
+    }
+
+    if (customerCode) {
+      query.customerCode = customerCode;
+    }
+
+    const bols = await Bols.find(query)
+      .limit(+limit)
+      .skip(+limit * (+page - 1))
+      .sort([["updatedAt", -1]]);
+
+    const { status: spreadStatus, ...allQuery } = query;
+
+    const totalBol = await Bols.countDocuments(allQuery);
+    const totalNew = await Bols.countDocuments({
+      ...query,
+      status: BOL_STATUS_ENUM.NEW,
+    });
+    const totalRefund = await Bols.countDocuments({
+      ...query,
+      status: BOL_STATUS_ENUM.REFURNING,
+    });
+    const totalFinish = await Bols.countDocuments({
+      ...query,
+      status: BOL_STATUS_ENUM.FINISHED,
+    });
+    const totalUnsuccess = await Bols.countDocuments({
+      ...query,
+      status: BOL_STATUS_ENUM.UNSCCESSFUL,
+    });
+    return {
+      totalBol,
+      totalRefund,
+      totalNew,
+      totalFinish,
+      totalUnsuccess,
+      bols,
+    };
+  };
+
+  store = async (payload) => {
+    const startDate = moment(payload.startDate).format("YYYY-MM-DD HH:mm");
+    const endDate = payload.endDate
+      ? moment(payload.startDate).format("YYYY-MM-DD HH:mm")
+      : null;
+
+    const currentCustomer = await Customers.findOne({
+      _id: payload.customerId,
+    });
+
+    const convertPayload = {
+      ...payload,
+      customerCode: currentCustomer.code,
+      customerName: currentCustomer.name,
+      startDate,
+      endDate,
+    };
+
+    const bol = await Bols.create(convertPayload);
+    return bol;
+  };
+
+  update = async (id, payload) => {
+    const startDate = moment(payload.startDate)
+      .subtract(1, "day")
+      .format(FORMAT_DATE.YMDHm);
+    const endDate = payload?.endDate
+      ? moment(payload.endDate).format(FORMAT_DATE.YMDHm)
+      : null;
+
+    const currentCustomer = await Customers.findOne({
+      _id: payload.customerId,
+    });
+
+    const convertPayload = {
+      ...payload,
+      customerCode: currentCustomer.code,
+      customerName: currentCustomer.name,
+      startDate,
+      endDate,
+    };
+    const bolDetail = await Bols.findOneAndUpdate(
+      { _id: id },
+      { $set: convertPayload },
+      { upsert: true }
+    );
+    return bolDetail;
+  };
+
+  upload = async (file) => {
+    const customerList = await Customers.find({});
+
+    const workBook = XLSX.read(file, { cellDates: true });
+    const wordSheet = workBook.Sheets[workBook.SheetNames[0]];
+    const countRow = XLSX.utils.sheet_to_json(wordSheet);
+    const arrayPayload = [];
+
+    for (let index = 2; index <= countRow.length + 1; index++) {
+      const startDate = wordSheet[`A${index}`]?.v || moment().format();
+      const code = wordSheet[`B${index}`]?.v || "";
+      const customerCode = wordSheet[`C${index}`]?.v || "";
+      const receivedName = wordSheet[`D${index}`]?.v || "";
+      const receivedPhoneNumber = wordSheet[`E${index}`]?.v || "";
+      const address = wordSheet[`F${index}`]?.v || "";
+      const category = wordSheet[`G${index}`]?.v || "";
+      const quantity = wordSheet[`H${index}`]?.v || 1;
+      const convertCategoryList = category.split("+") || [];
+
+      const categoryAfterConvertToObject = convertCategoryList?.reduce(
+        (acc, category) => {
+          const findCategory = CATEGORY_LIST.find((item) =>
+            item.code.includes(category.toUpperCase())
+          );
+          if (findCategory) {
+            return [...acc, findCategory];
+          }
+          return acc;
+        },
+        []
+      );
+
+      const currentCustomer = customerList.find((c) => c.code === customerCode);
+
+      arrayPayload.push({
+        code,
+        category: categoryAfterConvertToObject,
+        address,
+        quantity,
+        from: "2/10 Hồng Hà,p2,Tân Bình,HCM",
+        path: "",
+        description: "",
+        receivedName,
+        receivedPhoneNumber,
+        startDate: moment(startDate)
+          .tz(UTC_TIMEZONES)
+          .format("YYYY-MM-DD HH:mm"),
+        customerCode,
+        customerId: `${currentCustomer?._id}`,
+        customerName: currentCustomer?.name,
+        status: 0,
+      });
+    }
+
+    const bols = await Bols.insertMany(arrayPayload);
+    return bols;
+  };
+}
+export const BolServices = new BolServices();
